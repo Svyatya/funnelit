95a8c633354611cff9577e5e923ae2451a2b1e53
Svyatya <svblbl@yandex.ru>
2020-07-21 00:56:06 +0300

    Updates

---
+++ precache-manifest.6ed86f0912928f264e432a0ded278b9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f546d4e2395d3ca5df90bed094d946",
+    "url": "/funnelit/index.html"
+  },
+  {
+    "revision": "3af4051b9b2dd10a4894",
+    "url": "/funnelit/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "745d7c9ee94917a5fb77",
+    "url": "/funnelit/static/css/main.ad7fc027.chunk.css"
+  },
+  {
+    "revision": "3af4051b9b2dd10a4894",
+    "url": "/funnelit/static/js/2.9d573e01.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/funnelit/static/js/2.9d573e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745d7c9ee94917a5fb77",
+    "url": "/funnelit/static/js/main.4e1ad1d0.chunk.js"
+  },
+  {
+    "revision": "573b55ff3e7940a0ee29",
+    "url": "/funnelit/static/js/runtime-main.e2cb8f58.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/funnelit/precache-manifest.162f1dcf482d76f6538040f24e6bbab4.js"
+  "/funnelit/precache-manifest.6ed86f0912928f264e432a0ded278b9d.js"
 );
 
 self.addEventListener('message', (event) => {
